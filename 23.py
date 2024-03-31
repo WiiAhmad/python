10c1a3fe7b561353b17c80167a6a16451561c194
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2024-03-31 15:00:00 +0700

    lanjut

--- 23.py
+++ 23.py
@@ -1,1 +1,16 @@
 #percabangan latihan
+
+print("program kalkulator sederhana dengan 2 input")
+
+angka1 = float(input("masukan angka pertama : "))
+operator = input("masukan operator : ")
+angka2 = float(input("masukan angka kedua : "))
+
+print(f"angka pertama {angka1}, angka kedua {angka2}")
+
+if operator == "+":
+    hasil = angka1 + angka2
+    print(f"jawaban adalah {hasil}")
+elif operator == "-":
+    hasil = angka1 - angka2
+    print(f"jawaban adalah {hasil}")
---
+++ 24.py
@@ -0,0 +1,8 @@
+#loop
+
+angka = [0,1,2,3,4]
+print(angka)
+
+for i in angka:
+    print(f"i sekarang = {i}")
+
